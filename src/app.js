5a8dd25b1be3e6d763800fd239b2d87bee438797
Riccardo Barone <riccardo.ComputerScience@gmail.com>
2024-10-01 22:38:07 +0100

    #01- created connection to database and connection to port (server)

---
+++ src/app.js
@@ -0,0 +1,21 @@
+require("dotenv").config(); // Load environment variables from .env
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const connectToDatabase = require ('./db');
+
+const app = express();
+app.use(express.json());
+
+// simple route to test server
+app.get("/", (_req, res) => {
+    res.send("Hello world");
+});
+
+connectToDatabase();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`); // Log the server status
+});
