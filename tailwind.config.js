3d0045a823c1e196491730197beab95593f721d3
Riccardo Barone <riccardo.computerscience@gmail.com>
2024-11-05 12:31:07 +0000

    #25 refactoring of header and navbar. ongoing implementatio of navbar animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./**/*.{html,ejs}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        growDown: {
+          '0%': {transform: 'scaleY(0)'},
+          '80%': {transform: 'scaleY(1.1)'},
+          '100%': {transform: 'scaleY(1)'},
+        }
+      },
+      animation: {
+        growDown: 'growDown 0.3s ease-out forwards'
+      }
+    },
   },
   plugins: [],
 }
