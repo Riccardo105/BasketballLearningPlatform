5d0a73134495eb14433ea6fe89e78aadf53dec1e
Riccardo Barone <riccardo.ComputerScience@gmail.com>
2024-11-05 00:52:04 +0000

    # completed implementation of dynamicism of nav bar

--- public/js/toggleNavMenu.js
+++ public/js/toggleNavMenu.js
@@ -1,18 +1,30 @@
 
-function toggleMenu(menuId) {
+document.addEventListener("DOMContentLoaded", function () {
+    function toggleMenu(menuId) {
 
-    const menu1 = document.getElementById("burgerMenu");
-    const menu2 = document.getElementById("profileMenu");
+        const menu1 = document.getElementById("burgerMenu");
+        const menu2 = document.getElementById("profileMenu");
 
-    if (menuId === menu1) {
-        menu1.classList.toggle("hidden");
-        menu2.classList.add("hidden");
-    } else if (menuId === menu2) {
-        menu1.classList.add("hidden");
-        menu2.classList.toggle("hidden");
-    }
-}
+        if (menuId === "burgerMenu") {
+            menu1.classList.toggle("hidden");
+            menu2.classList.add("hidden");
+        } else if (menuId === "profileMenu") {
+            menu1.classList.add("hidden");
+            menu2.classList.toggle("hidden");
+        }
+    };
 
+    const burgerIcon = document.querySelector(".fa-bars");
+    const profileIcon = document.querySelector(".fa-user");
+
+    burgerIcon.addEventListener("click", function () {
+        toggleMenu("burgerMenu");
+    });
+
+    profileIcon.addEventListener("click", function () {
+        toggleMenu("profileMenu");
+    });
+});
 
 
 
